4e40e8cd2cfe3db6722af1d38c98c23e115e802e
Absintu <joaosf7@gmail.com>
2020-10-24 19:53:16 +0100

    centered header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react'
 import {Image} from 'semantic-ui-react'
 import title from './title.svg'
+import './Header.css'
 //import {Link} from '../routes'
 
 class Header extends Component {
     render(){
         return(
-            <div><Image src={title} size='small' /></div>
+            <div className='Header'><Image src={title} size='small' /></div>
         );
     }
 }
