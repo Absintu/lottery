a24bf0e4dd6159778d7d340bac684ddb4f6069da
Absintu <joaosf7@gmail.com>
2020-10-24 02:10:33 +0100

    Fixed web3 and metamask integration

--- src/web3.js
+++ src/web3.js
@@ -1,25 +1,11 @@
 import Web3 from 'web3'
 
-const web3 = new Web3(window.ethereum);
-
-if (window.ethereum) {
-   try {
-   	web3 = new Web3(window.ethereum)
-       window.ethereum.enable().then(function() {
-          // User has allowed account access to DApp...
-      });
-   } catch(e) {
-      // User has denied account access to DApp...
-      alert('Cant use window.ethereum !');
-   }
-}
-// Legacy DApp Browsers
-else if (window.web3) {
-    web3 = new Web3(window.web3.currentProvider);
-}
-// Non-DApp Browsers
-else {
-    alert('You have to install MetaMask !');
-}
-
+const web3 = () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      window.ethereum.enable();
+      return true;
+    }
+    return false;
+  }
 export default web3;
