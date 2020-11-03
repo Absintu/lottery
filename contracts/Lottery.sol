155e4261db8cf9e1bddb59dbda1419b7ac02d53b
Absintu <joaosf7@gmail.com>
2020-11-03 08:31:45 +0000

    Add v0.2.0 with arrays

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -10,7 +10,7 @@ contract Lottery{
   Player[] public players;
   address public lastWinner;
   address payable[] addArray;
-  address payable[] public fullAddArray;
+  uint public numberOfTickets = 0;
 
   constructor() public{
     manager = msg.sender;
@@ -21,27 +21,32 @@ contract Lottery{
     Player memory player = Player({add: msg.sender, amount: msg.value});
     players.push(player);
     addArray.push(msg.sender);
+    uint temp = msg.value/(1e16);
+    numberOfTickets += temp;
   }
 
-  function random() private view returns(uint){
-    return uint(keccak256(abi.encodePacked(block.difficulty, now, fullAddArray))); // ou sha3();
+  function random(address payable[] memory _fullAddArray) private view returns(uint){
+    return uint(keccak256(abi.encodePacked(block.difficulty, now, _fullAddArray))); // ou sha3();
     // Eu poderia melhorar esta funcao injectando o clock da maquina, por ex
     // Esta funcoa nao e muito segura para gerar um numero aleatorio
   }
 
   function pickWinner() public restricted{
     uint i;
+    uint indexFullAddArray=0;
+    address payable[] memory fullAddArray = new address payable[](numberOfTickets);
     for(i=0; i<players.length; i++){
       uint j;
       for(j=0.01 ether; j<=players[i].amount; j += 0.01 ether){
-        fullAddArray.push(players[i].add);
+        fullAddArray[indexFullAddArray++] = players[i].add;
       }
     }
-    uint index = random() % fullAddArray.length;
+    uint index = random(fullAddArray) % fullAddArray.length;
     fullAddArray[index].transfer(address(this).balance);
     lastWinner = fullAddArray[index];
     delete fullAddArray;
     delete players;
+    numberOfTickets = 0;
   }
 
   modifier restricted(){
@@ -51,13 +56,5 @@ contract Lottery{
 
   function getPlayers() public view returns(address payable[] memory){
     return addArray;
-  }
-
-  function getPlayerss() public view returns(address){
-    return players[1].add;
-  }
-
-  function getFullAdd() public view returns(address payable[] memory){
-    return fullAddArray;
   }
 }
